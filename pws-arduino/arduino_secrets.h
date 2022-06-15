fc013f763a28e7311d085f340809c69b30db7e30
Karl <karl.bittner@hexaquo.at>
2022-06-15 15:09:12 +0200

    Update Arudino secrets template

--- pws-arduino/arduino_secrets.h
+++ pws-arduino/arduino_secrets.h
@@ -1,2 +1,9 @@
-#define SECRET_SSID "ENTER_WIFI_SSID"
-#define SECRET_PASS "ENTER_WIFI_PASSWORD"
+// WiFi
+#define SECRET_SSID ""
+#define SECRET_PASS ""
+
+// GSM
+#define GSM_PIN_NUMBER "5"
+#define GPRS_APN ""
+#define GPRS_LOGIN ""
+#define GPRS_PASSWORD ""
